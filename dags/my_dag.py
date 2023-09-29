b2a871332fd66aeccde916805b8308fd89936ec4
JoNa <jona@JoNas-MacBook-Pro.local>
2023-09-28 20:58:51 -0500

    Creando un DAG, con tareas y dependencia de tareas

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,13 +1,18 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from include.helpers import print_msg
 from datetime import datetime
 
+def print_a():
+    print('hi fron task a')
+
+def print_b():
+    print('hi fron task b')
+
 with DAG ('my_dag', start_date=datetime(2022,1,1),
+          description = 'A simple tutorial DAG', tags =['data_science'],
           schedule_interval='@daily', catchup=False) as dag:
 
-    task = PythonOperator(
-        task_id='task',
-        python_callable=print_msg
+    task_a = PythonOperator(task_id='task_a',python_callable=print_a )
+    task_b = PythonOperator(task_id='task_b',python_callable=print_b )
 
-    )
+    task_a >> task_b
